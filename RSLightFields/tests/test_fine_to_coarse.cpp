a951280287b043b5036b856caa8a7af88ec30a1d
14chanwa <quentin.cwn@gmail.com>
2018-03-23 13:40:18 +0100

    Update report
    - Add figures & comparison
    - TODO: description of comparisons

--- RSLightFields/tests/test_fine_to_coarse.cpp
+++ RSLightFields/tests/test_fine_to_coarse.cpp
@@ -21,11 +21,11 @@ int main(int argc, char* argv[])
 
     
     // Load all images in folder
-    //~ std::vector<cv::Mat> list_mat = rslf::read_imgs_from_folder("../data/skysat_lasvegas_rectified/rectified_equalized_resized_frames_step18/", "tif", CV_LOAD_IMAGE_UNCHANGED);
+    std::vector<cv::Mat> list_mat = rslf::read_imgs_from_folder("../data/skysat_lasvegas_rectified/rectified_equalized_resized_frames_step18/", "tif", CV_LOAD_IMAGE_UNCHANGED);
     //~ std::vector<cv::Mat> list_mat = rslf::read_imgs_from_folder("../data/rectified_equalized_frames/", "png", CV_LOAD_IMAGE_UNCHANGED);
     //~ std::vector<cv::Mat> list_mat = rslf::read_imgs_from_folder("../data/skysat_lasvegas_rectified/rectified_equalized_resized_frames_step1/", "tif", CV_LOAD_IMAGE_UNCHANGED);
     //~ std::vector<cv::Mat> list_mat = rslf::read_imgs_from_folder("../data/sieleters_bande2_short/", "tif", CV_LOAD_IMAGE_UNCHANGED, true, true);
-    std::vector<cv::Mat> list_mat = rslf::read_imgs_from_folder("../data/mansion_image_resized/", "jpg", CV_LOAD_IMAGE_UNCHANGED);
+    //~ std::vector<cv::Mat> list_mat = rslf::read_imgs_from_folder("../data/mansion_image_resized/", "jpg", CV_LOAD_IMAGE_UNCHANGED);
     
     std::cout << list_mat.size() << " images read" << std::endl;
     
@@ -36,10 +36,10 @@ int main(int argc, char* argv[])
     //~ cv::waitKey();
     
     //~ float d_min = -2.0;
-    //~ float d_min = -1.0;
+    float d_min = -1.0;
     //~ float d_min = 0.3;
     //~ float d_min = -0.5;
-    float d_min = 0.0;
+    //~ float d_min = 0.0;
     //~ float d_max = 2.0;
     //~ float d_max = 8.0;
     float d_max = 4.0;
@@ -53,9 +53,9 @@ int main(int argc, char* argv[])
     
     std::cout << dim_d << " d values requested" << std::endl;
     
-    //~ rslf::FineToCoarse<float> fine_to_coarse(epis, d_min, d_max, dim_d);
+    rslf::FineToCoarse<float> fine_to_coarse(epis, d_min, d_max, dim_d);
     //~ rslf::FineToCoarse<float> fine_to_coarse(epis, d_min, d_max, dim_d, -1, rslf::Depth1DParameters<float>::get_default(), -1, true);
-    rslf::FineToCoarse<cv::Vec3f> fine_to_coarse(epis, d_min, d_max, dim_d);
+    //~ rslf::FineToCoarse<cv::Vec3f> fine_to_coarse(epis, d_min, d_max, dim_d);
     fine_to_coarse.run();
     
     std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
@@ -63,8 +63,8 @@ int main(int argc, char* argv[])
     std::cout << "Time elapsed: " << duration << " seconds" << std::endl;
     
     std::vector<cv::Mat> out_plot_depth_s_v_u_;
-    //~ fine_to_coarse.get_coloured_depth_maps_and_imgs(out_plot_depth_s_v_u_, colormap, saturate);
-    fine_to_coarse.get_coloured_depth_maps(out_plot_depth_s_v_u_, colormap, saturate);
+    fine_to_coarse.get_coloured_depth_maps_and_imgs(out_plot_depth_s_v_u_, colormap, saturate);
+    //~ fine_to_coarse.get_coloured_depth_maps(out_plot_depth_s_v_u_, colormap, saturate);
     std::vector<cv::Mat> out_plot_epi_pyr_p_s_u_;
     fine_to_coarse.get_coloured_epi_pyr(out_plot_epi_pyr_p_s_u_, -1, colormap, saturate);
     std::vector<cv::Mat> out_plot_depth_pyr_p_v_u_;
